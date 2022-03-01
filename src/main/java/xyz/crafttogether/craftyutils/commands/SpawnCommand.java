3518d6d393ad69e88c0c562d28fd63d2010fafdd
PolarianDev <polariandev@protonmail.com>
2022-03-01 11:44:57 +0000

    spawn command with hopefully working warmup

--- src/main/java/xyz/crafttogether/craftyutils/commands/SpawnCommand.java
+++ src/main/java/xyz/crafttogether/craftyutils/commands/SpawnCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import xyz.crafttogether.craftcore.minecraft.utils.WarmupHandler;
 
 public class SpawnCommand implements CommandExecutor {
     @Override
@@ -16,10 +17,17 @@ public class SpawnCommand implements CommandExecutor {
             commandSender.sendMessage(ChatColor.RED + "You must run this command as a player");
             return true;
         }
-        World world = Bukkit.getWorld("world");
-        assert world != null;
-        player.teleport(world.getSpawnLocation());
-        player.sendMessage(ChatColor.GREEN + "Successfully teleported you to spawn");
+        player.sendMessage(ChatColor.GREEN + "Please stand still for 5 seconds...");
+        WarmupHandler.schedule(player.getUniqueId(), 5, (successful) -> {
+            if (successful) {
+                World world = Bukkit.getWorld("world");
+                assert world != null;
+                player.teleport(world.getSpawnLocation());
+                player.sendMessage(ChatColor.GREEN + "Successfully teleported you to spawn");
+            } else {
+                player.sendMessage(ChatColor.RED + "You have moved, warmup cancelled");
+            }
+        });
         return true;
     }
 }
